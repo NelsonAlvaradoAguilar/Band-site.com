28ab048147e72e9a1f963b8c76a85beb1b471fb8
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2023-12-03 11:58:21 -0500

    showsPG: style, scripts, Assets & mobile added

---
+++ scripts/index-page.js
@@ -0,0 +1,8 @@
+//let header__logo = document.querySelector(".header__logo")
+//header__logo.innerHTML = "chesse"//
+//console.log(header__logo)
+//let comments__posted = document.querySelector("comments__posted")
+//comments__posted.innerHTML = ""
+
+
+
