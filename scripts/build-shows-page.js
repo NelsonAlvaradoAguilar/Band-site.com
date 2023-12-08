970b73c89b864aabd523b7e4f34194b696be9a0c
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2023-12-08 17:14:21 -0500

    shows PG:correction,scripts. shows section added through DOM. iframe song added with no Styles. scss for shows tour section. bio PG:correction. default comments array. re.render Function working on it.

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,103 @@
+let showsArray = [{
+    date: 'Mon Sept 06 2021',
+    venue: 'Ronal Lane',
+    location: 'San Francisco,CA'
+},
+{
+    date: 'Tue Sept 21 2021',
+    venue: 'Ronal Lane',
+    location: 'San Francisco,CA'
+},
+{
+    date: 'Fri Oct 15 2021',
+    venue: 'View Lounge',
+    location: 'San Francisco,CA'
+},
+{
+    date: 'Sat Nov 06 2021',
+    venue: 'Hyatt Agency',
+    location: 'San Francisco,CA'
+},
+{
+    date: 'Fri Nov 26 2021',
+    venue: 'Moscow Center',
+    location: 'San Francisco,CA'
+},
+{
+    date: 'Wed Dec 15 2021',
+    venue: 'Press Club',
+    location: 'San Francisco,CA'
+}
+
+
+]
+
+const $showsContainer = document.getElementById('showsContainer');
+$showsContainer.classList.add('shows');
+const $title = document.createElement('h1');
+$showsContainer.appendChild($title);
+$title.insertAdjacentText('beforeend', 'Shows');
+let displayShows = function () {
+
+
+    showsArray.forEach(e => {
+
+        const $ul = document.createElement('ul'),
+            $liDate = document.createElement('li'),
+            $spanDate = document.createElement('span'),
+            $liVenue = document.createElement('li'),
+            $spanVenue = document.createElement('span'),
+            $liLocation = document.createElement('li'),
+            $spanLocation = document.createElement('span'),
+            $buyButton = document.createElement('a');
+          //  $devider = document.createElement("div");
+
+        $showsContainer.appendChild($ul);
+       
+        $ul.appendChild($liDate);
+        $ul.appendChild($spanDate);
+        $ul.appendChild($liVenue);
+        $ul.appendChild($spanVenue);
+        $ul.appendChild($liLocation);
+        $ul.appendChild($spanLocation);
+        $ul.appendChild($buyButton);
+     //  $ul.insertAdjacentElement('afterend', $devider);
+
+        $liDate.insertAdjacentHTML('afterbegin', 'Date');
+        $spanDate.insertAdjacentHTML('afterbegin', `${e.date}`);
+        $liVenue.insertAdjacentHTML('beforeend', 'Venue');
+        $spanVenue.insertAdjacentHTML('beforeend', `${e.venue}`);
+        $liLocation.insertAdjacentHTML('beforeend', 'Location');
+        $spanLocation.insertAdjacentHTML('beforeend', `${e.location}`);
+        $buyButton.insertAdjacentText('beforeend',"BUY TICKETS")
+        //adding classname//
+        $liDate.classList.add("shows__subtitle");
+        $spanDate.classList.add("shows__date-info");
+
+        $liVenue.classList.add("shows__subtitle");
+        $spanVenue.classList.add("shows__venue-info");
+
+        $liLocation.classList.add("shows__subtitle");
+        $spanLocation.classList.add("shows__location-info");
+
+        $buyButton.classList.add("shows__btnbuy")
+
+        $ul.classList.add("shows__tour")
+       // $devider.classList.add("shows__devider")
+        $showsContainer.appendChild($ul)
+    });
+}
+$title.classList.add('shows__title');
+const $iframe = document.getElementById("iframe");
+$iframe.classList.add("songToPlay")
+
+
+displayShows();
+
+
+
+
+
+
+
+
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,68 @@
-//let header__logo = document.querySelector(".header__logo")
-//header__logo.innerHTML = "chesse"//
-//console.log(header__logo)
-//let comments__posted = document.querySelector("comments__posted")
-//comments__posted.innerHTML = ""
 
 
+
+
+
+let commentsArray = [{
+    date: '02/17/2021',
+    username: 'Connor Walton',
+    text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+
+
+}, {
+    date: '01/09/2021',
+    username: 'Emilie Beach',
+    text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+
+
+}, {
+    date: '12/20/2020',
+    username: 'Miles Acosta',
+    text: 'I can not stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can not get enough.',
+
+
+}
+
+
+];
+
+let displayComments = function () {
+    const commentsContainer = document.getElementById('commentsContainer');
+
+    commentsContainer.insertAdjacentHTML('afterbegin', '')
+
+
+    commentsArray.forEach(comment => {
+
+        const commentElement = document.createElement('div'),
+
+            $titleDiv = document.createElement('div'),
+            $name = document.createElement('h1'),
+            $date = document.createElement('p'),
+            $paragraphComment = document.createElement('p'),
+            $divider =document.createElement('div');
+
+        $titleDiv.appendChild($name),
+            $titleDiv.appendChild($date),
+            commentElement.appendChild($titleDiv),
+            commentElement.appendChild($paragraphComment);
+           commentElement.appendChild($divider)
+
+        $name.insertAdjacentHTML('afterbegin', `${comment.username} `),
+            $date.insertAdjacentHTML('beforeend', `${comment.date}`),
+            $paragraphComment.insertAdjacentHTML('beforeend', `${comment.text}`);
+
+
+        commentsContainer.appendChild(commentElement);
+       
+
+        $divider.classList.add("block-interaction")
+       
+    });
+
+
+}
+
+
+displayComments();
 
