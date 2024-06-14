61a26f2dcc4562f9c6d558b72f18eb5dfe4bb622
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-06-14 17:20:20 -0400

    adding delete button

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,58 +1,39 @@
-
-let subTitles=['Date','Venue','Location']
+let subTitles = ["Date", "Venue", "Location"];
 import { bandSiteApi } from "./band-site-api.js";
 
+const showsContainer = document.getElementById("showsContainer");
+showsContainer.classList.add("shows");
+const title = document.createElement("h1");
+showsContainer.insertAdjacentElement("beforebegin", title);
+title.insertAdjacentText("beforeend", "Shows");
+const subTitleBand = document.createElement("div");
 
-const showsContainer = document.getElementById('showsContainer');
-showsContainer.classList.add('shows')
-const title = document.createElement('h1');
-showsContainer.insertAdjacentElement('beforebegin', title)
-title.insertAdjacentText('beforeend', 'Shows');
-const subTitleBand = document.createElement('div');
-
-
- let renderTitles = function () {
-    for (let i = 0; i < subTitles.length; i++) {
-
-        const h3 = document.createElement('h3');
-        subTitleBand.appendChild(h3);
-        h3.insertAdjacentText('beforeend', subTitles[i]);
-        showsContainer.insertAdjacentElement('beforebegin', subTitleBand);
-        subTitleBand.classList.add('shows__new-subtitle')
-        h3.classList.add('shows__new-subtitle--hiden')
-
-    }
-
-}
+let renderTitles = function () {
+  for (let i = 0; i < subTitles.length; i++) {
+    const h3 = document.createElement("h3");
+    subTitleBand.appendChild(h3);
+    h3.insertAdjacentText("beforeend", subTitles[i]);
+    showsContainer.insertAdjacentElement("beforebegin", subTitleBand);
+    subTitleBand.classList.add("shows__new-subtitle");
+    h3.classList.add("shows__new-subtitle--hiden");
+  }
+};
 renderTitles();
 
-document.addEventListener('click', () => {
-    const showsList = document.querySelectorAll('.shows__list');
+document.addEventListener("click", () => {
+  const showsList = document.querySelectorAll(".shows__list");
 
-    showsList.forEach(i => {
-        i.addEventListener('click', function () {
-            showsList.forEach(el => el.classList.remove('shows__list--selected'));
-            i.classList.toggle('shows__list--selected');
-        });
+  showsList.forEach((i) => {
+    i.addEventListener("click", function () {
+      showsList.forEach((el) => el.classList.remove("shows__list--selected"));
+      i.classList.toggle("shows__list--selected");
     });
+  });
 });
 
+let getShowlist = bandSiteApi.getShows();
 
-let getShowlist= bandSiteApi.getShows();
-
-
-title.classList.add('shows__title');
+title.classList.add("shows__title");
 const iframe = document.getElementById("iframe");
 iframe.classList.add("hero__songToPlay");
 iframe.setAttribute("loading", "eager");
-
-
-
-
-
-
-
-
-
-
-
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,32 +1,33 @@
-
 import { bandSiteApi } from "./band-site-api.js";
-const commentform = document.getElementById('commentsForm');
+const commentform = document.getElementById("commentsForm");
 class comment {
-    constructor(name, comment) {
-        this.name = name;
-        this.comment = comment;
-    }
+  constructor(name, comment) {
+    this.name = name;
+    this.comment = comment;
+  }
 }
-commentform.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const formNameVal = event.target.username.value;
-    const formTextVal = event.target.usertext.value;
+commentform.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formNameVal = event.target.username.value;
+  const formTextVal = event.target.usertext.value;
 
-    if (formNameVal !== '' && formTextVal !== '') {
-        try {
+  if (formNameVal !== "" && formTextVal !== "") {
+    try {
+      let newComment = new comment(formNameVal, formTextVal);
+      bandSiteApi.postComment(newComment);
+      console.log(newComment);
 
-            let newComment = new comment(formNameVal, formTextVal);
-            bandSiteApi.postComment(newComment)
-            console.log(newComment);
-
-            event.target.reset();
-        } catch (error) {
-            console.log(error);
-        }
-
+      event.target.reset();
+    } catch (error) {
+      console.log(error);
     }
-
+  }
 });
 
+const deleteComment = document.getElementsByClassName(
+  "comments__delete-comment"
+);
+
+console.log(deleteComment);
 
 let CommentsPosted = bandSiteApi.getComments();
